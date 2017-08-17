0a747fe185f28464539e52c4e4ee690e1281a438
sunnycal <milana.campbell@gmail.com>
2017-08-17 16:46:21 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/DirectorRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+import com.example.demo.Director;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DirectorRepository extends CrudRepository<Director, Long>{
+}
+
